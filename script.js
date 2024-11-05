8978df034bb3b5b094e7b3a3fe30f53a48b65c80
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-11-05 14:52:07 +0530

    Make JS operation functions and basic HTML buttons

---
+++ script.js
@@ -0,0 +1,32 @@
+let num1;
+let num2;
+let operator;
+
+function operate(number1, number2, operation) {
+  switch (operation) {
+    case "+":
+      return add(number1, number2);
+    case "-":
+      return subtract(number1, number2);
+    case "*":
+      return multiply(number1, number2);
+    case "/":
+      return divide(number1, number2);
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
